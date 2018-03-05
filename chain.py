706947cf05239f51033da1b08fb9b57a0844d3a1
pinealan <achan961117@gmail.com>
2018-03-05 06:16:47 +0000

    Add RSA asymmetric key cryptography
    Use RSA keys for signing and verifying transactions
    Use RSA public key for address identification
    Document transaction data format, including signatures

--- chain.py
+++ chain.py
@@ -3,65 +3,143 @@
 TODO:
     Implement client
     Implement server
-    Consider generating address from private keys
-    Consider private key cryptography for Tx verification
-    Consider hash object as abstraction over hash representation (like hashlib)
-    Separate hash byte representations and int representations
 '''
 
 import functools
 import hashlib
 import time
 
-from Crypto.Hash      import SHA
+from Crypto.Hash      import SHA as sha
 from Crypto.PublicKey import RSA
 from Crypto.Signature import pss
 
 
-# @Refacotr: Use proper functions
-HASH = lambda x: abs(hash(x))
-TIME = lambda: int(time.time() * 1000) # millisecond
+def HASH(*args):
+    h = sha.new()
+    for arg in args:
+        if isinstance(arg, str):
+            arg = arg.encode()
+        elif isinstance(arg, int):
+            arg = arg.to_bytes(length=128, byteorder='big')
+        elif not isinstance(arg, bytes):
+            raise TypeError("cannot hash '{}' object".format(type(arg)))
+        h.update(arg)
+    return h
+
+
+def tree_hash(*args):
+    return HASH(*args).hexdigest()
+
+
+def key_to_public_hex(key):
+    return key.exportKey('DER').hex()
+
+
+def key_to_private_hex(key):
+    return key.publickey().exportKey('DER').hex()
+
+
+def public_hex_to_key(pub):
+    return RSA.importKey(bytes.fromhex(pub))
+
+
+def private_hex_to_key(priv):
+    return RSA.importKey(bytes.fromhex(priv))
+
+
+def now_in_millisecond():
+    return int(time.time() * 1000)
 
 
 class Tx:
-    '''A transaction.
+    '''A transaction object.
+
+    Each transaction is initiated with 4 data fields:
+    [sender, reciever, amount, msg]
+
+    The signature of a Tx is created by signing off the hash digest of these 4
+    data fields, with the private key that matches the sender's public address.
+    Together this results in 5 data fields which goes into the final hash ID of
+    a transaction.
+
+    The final transaction hash is created with 5 data fields
+    [sender, reciever, amount, signature, msg]
+
+    Args:
+        by, to: Public address (Hex) of sender and reciever of this transaction
+        amount: Value being transacted
+        msg (optional): An string message attached to this transaction
 
     Attributes:
-        by, to: Sender and reciever of this transaction
-        amount: Value of credit being transacted
-        msg (optional): A string
+        by, to: Public address (Hex) of sender and reciever of this transaction
+        amount: Value being transacted
+        signature: Signature by sender, signed with private key
+        msg (optional): An string message attached to this transaction
+        _prehash: Hash object of this Tx
+        _hash: Hash object of this Tx
     '''
-    def __init__(self, by, to, amount, timestamp, msg=0):
+    def __init__(self, by, to, amount, msg=0):
         self.by         = by
         self.to         = to
         self.amount     = amount
-        self.timestamp  = timestamp
         self.msg        = msg
+        self.signature  = None
+        self._prehash   = HASH(by, to, amount, msg)
+        self._hash      = None
+
+
+    def isSigned(self):
+        return self.signature is not None
+
+
+    def isValid(self):
+        if not self.isSigned():
+            return False
+        try:
+            verifier = pss.new(public_hex_to_key(self.by))
+            verifier.verify(self._prehash, self.signature)
+            return True
+        except ValueError:
+            return False
+
+
+    def sign(self, key):
+        '''Sign this transaction.'''
+        self.signature = pss.new(key).sign(self._prehash)
+        self._hash = HASH(self.by, self.to, self.amount, self.signature, self.msg)
+        return self
 
 
     def __hash__(self):
-        '''Memotization of hash value'''
-        try:
-            return self._hash
-        except AttributeError:
-            self._hash = HASH((
-                self.by,
-                self.to,
-                self.amount,
-                self.timestamp,
-                self.msg
-            ))
-            return self._hash
+        '''Return int representation of hash digest.'''
+        return hash(self._hash.digest())
+
+
+    @property
+    def hash(self):
+        '''Return binary representation of hash.'''
+        return self._hash.digest()
+
+
+    @property
+    def hexhash(self):
+        '''Return hex representation of hash'''
+        return self._hash.hexdigest()
 
 
     def __repr__(self):
-        return 'Tx(Hash: {}, Sender: {}, Receiver: {}, Amount: {}, Time: {})'.format(
-                self.__hash__(), self.by, self.to, self.amount, self.timestamp)
+        try:
+            hexhash = self.hexhash[:8]
+            sign    = self.signature.hex()[:8]
+        except:
+            hexhash = None
+            sign    = None
+        return '{}(hash: {}, sign: {})'.format(
+                self.__class__.__name__, hexhash, sign)
 
 
-    @classmethod
-    def hash_txs(*txs):
-        return functools.reduce(lambda x, y: HASH((x, y)), txs)
+def hash_txs(*txs):
+    return functools.reduce(lambda x, y: tree_hash(x, y), (tx.hash for tx in txs))
 
 
 
@@ -79,37 +157,26 @@ class BlockHeader:
         self.roothash   = roothash
         self.timestamp  = timestamp
         self.nouce      = nouce
-        self._hash      = None
+        self._hash      = HASH(prevhash, roothash, timestamp, nouce)
 
 
     def __hash__(self):
-        '''Memotization of hash value'''
-        if not self._hash:
-            self._hash = HASH((
-                self.prevhash,
-                self.roothash,
-                self.timestamp,
-                self.nouce
-            ))
-        return self._hash
+        return hash(self._hash)
 
 
     @property
     def hash(self):
-        return hash(self)
+        '''Memotization of hash value'''
+        return self._hash.digest()
 
 
     @property
     def hexhash(self):
-        raise NotImplementedError
+        return self._hash.hexdigest()
 
 
     def __repr__(self):
-        return '{}({})'.format(
-                self.__class__.__name__,
-                self.hash,
-                self.prevhash,
-                self.timestamp)
+        return '{}(hash: {})'.format(self.__class__.__name__, self.hash)
 
 
 
@@ -121,7 +188,9 @@ class Block:
         transactions: List of all transactions
     '''
     def __init__(self, prevhash, roothash, timestamp, nouce, transactions=None):
-        self.header = BlockHeader(prevhash, roothash, timestamp, nouce)
+        self.header       = BlockHeader(prevhash, roothash, timestamp, nouce)
+        self.hash         = self.header.hash
+        self.hexhash      = self.header.hexhash
         self.transactions = transactions or []
 
 
@@ -134,7 +203,7 @@ class Block:
 
 
     def __repr__(self):
-        return '{}({})'.format(self.__class__.__name__, hash(self))
+        return '{}(hash: {})'.format(self.__class__.__name__, hash(self))
 
 
 
@@ -157,51 +226,42 @@ class Chain:
         return self.addr is not None
 
 
-    def isValidTx(self, tx):
+    def validateTx(self, tx):
         '''Reject transaction if sender does not have enough credit.'''
-        try:
-            if self.addr[tx.by] < tx.amount:
-                return False
-            else:
-                return True
-        except KeyError:
-            return False
+        return tx.isValid() and self.addr[tx.by] >= tx.amount
 
 
-    def isValidBlock(self, block, parent_block):
-        valid_hash = block.prevhash == hash(parent_block)
+    def validateBlock(self, block, parent_block):
+        valid_hash = block.prevhash == parent_block.hexhash
         valid_time = block.timestamp >= parent_block.timestamp
         return valid_hash and valid_time
 
 
-    def transact(self, by, to, amount, timestamp, msg=0):
+    def transact(self, key, to, amount, msg=0, by=None):
         '''Record transaction on chain.'''
-        tx = Tx(by, to, amount, timestamp, msg)
+        by = by or key_to_public_hex(key)
+        tx = Tx(by, to, amount, msg).sign(key)
 
-        if not self.isValidTx(tx):
-            raise ValueError
+        if not self.validateTx(tx):
+            raise ValueError('invalid tx')
 
-        self.addr[tx.by] -= amount
         try:
+            self.addr[tx.by] -= amount
             self.addr[tx.to] += amount
         except KeyError:
             self.addr[tx.to] = amount
 
-        self.txs.append(Tx)
+        self.txs.append(tx)
 
         if len(self.txs) == self.tx_limit:
-            self.makeBlock(timestamp)
+            self.addBlock(now_in_millisecond())
 
 
-    def makeBlock(self, timestamp):
-        root_hash = Tx.hash_txs(*self.txs)
-        self.blocks.append(Block(self.last_hash, root_hash, timestamp, 0, self.txs))
+    def addBlock(self, timestamp):
+        root_hash = hash_txs(*self.txs)
+        new_block = Block(self.last_block.hexhash, root_hash, timestamp, 0, self.txs)
+        self.blocks.append(new_block)
         self.txs = []
-
-
-    @property
-    def last_hash(self):
-        return hash(self.blocks[-1])
 
 
     @property
@@ -210,19 +270,35 @@ class Chain:
 
 
 # Demo
+for k in 'abcde':
+    key = [RSA.importKey(open(k + '.der', 'br').read()) for k in 'abcde']
+    pri = [key_to_private_hex(k) for k in key]
+    pub = [key_to_public_hex(k) for k in key]
+
+
 addr = {
-    0: 100,
-    1: 200,
-    2: 50
+    pub[0]: 100,
+    pub[1]: 250,
+    pub[2]: 50
 }
+
+
 c = Chain(timestamp=0, addr=addr)
-c.transact(1, 2, amount=50, timestamp=10)
-c.transact(0, 3, amount=10, timestamp=11)
-c.transact(0, 4, amount=10, timestamp=12)
-c.transact(1, 5, amount=10, timestamp=13)
-c.transact(1, 6, amount=10, timestamp=14)
-c.transact(1, 7, amount=20, timestamp=14)
-c.transact(1, 8, amount=20, timestamp=14)
-c.transact(2, 9, amount=10, timestamp=14)
-c.transact(0, 3, amount=10, timestamp=8)
-c.transact(0, 4, amount=10, timestamp=5)
+
+c.transact(key[0], pub[1], amount=10)
+c.transact(key[0], pub[2], amount=40)
+
+c.transact(key[1], pub[2], amount=20)
+c.transact(key[1], pub[3], amount=10)
+c.transact(key[1], pub[3], amount=30)
+c.transact(key[1], pub[4], amount=50)
+
+c.transact(key[2], pub[4], amount=40)
+
+c.transact(key[3], pub[4], amount=10)
+c.transact(key[3], pub[2], amount=20)
+
+c.transact(key[4], pub[0], amount=20)
+c.transact(key[4], pub[2], amount=10)
+
+print(c.addr)
